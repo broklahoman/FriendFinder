2cafff732dbec3da02213807781024b82f54fb6b
Jonathan Harris <j.harris.onathan@gmail.com>
2017-04-22 23:34:44 -0500

    update

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,13 +2,13 @@ var data = require("../data/friends");
 
 
 module.exports = function(app) {
-
+	// when users hit a page display friend jsons from data folder
 	app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   	});
-
+	// push new friends from survey into friend array  
   	app.post("/api/friends", function(req, res) {
-  	res.json(friendsData);
+  	friendData.push(req.body);
 	});
 };
 
